6b1d7ebf3b2555d026665e77a44cef8516ee1d14
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-19 20:11:00 +0700

    circle-ci: Rebrand for change source
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 #
 # Copyright (C) 2019 @alanndz (Telegram and Github)
+# Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-export RELEASE_STATUS=0
-export USECLANG=1
-export USECACHE=0
-export CODENAME="Quantum"
-export KERNEL_VERSION="1.00"
-export TYPE_KERNEL="EAS"
+# I'll declare this alias in the other script
+#
+# export RELEASE_STATUS=0
+# export USECLANG=1
+# export USECACHE=0
+# export CODENAME="Quantum"
+# export KERNEL_VERSION="1.00"
+# export TYPE_KERNEL="EAS"
 
-git clone --depth=1 -b eas-q https://github.com/zxc070/kernel_xiaomi_lavender .
+# Well, let ci only download standalone bash file
+#
+# git clone --depth=1 -b eas-q https://github.com/zxc070/kernel_xiaomi_lavender .
 
-wget --output-document=.kernel.sh https://raw.githubusercontent.com/zxc070/scripts/master/ci/lavender/kernel.sh
+wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
 
-chmod +x .kernel.sh
-bash ./.kernel.sh
+chmod +x .circle-clang-mido.sh
+bash ./.circle-clang-mido.sh
