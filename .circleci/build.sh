04a9df7ee5fdcee6c1f40e5d0e08f50fca42fd1f
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-23 09:08:27 +0700

    circle-ci: test lavender
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,17 +4,6 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# I'll declare this alias in the other script
-#
-# export RELEASE_STATUS=0
-# export USECLANG=1
-# export USECACHE=0
-# export CODENAME="Quantum"
-# export KERNEL_VERSION="1.00"
-# export TYPE_KERNEL="EAS"
-
-git clone --depth=1 -b test/q-eas https://github.com/Nicklas373/kernel_xiaomi_lavender .
-
 wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
 
 chmod +x .circle-clang-lavender.sh
