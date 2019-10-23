e2175c846da86b0bf3e4078d3692a5387f61c6dc
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-23 11:04:20 +0700

    circle-ci: test mido
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
+wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
 
-chmod +x .circle-clang-lavender.sh
-bash ./.circle-clang-lavender.sh
+chmod +x .circle-clang-mido.sh
+bash ./.circle-clang-mido.sh
