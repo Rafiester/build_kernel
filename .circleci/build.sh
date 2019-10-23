9cd8a8c5bf56d85b0a07e6632102d2190e5c0dba
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-23 12:24:37 +0700

    circle-ci: fix lavender
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,9 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
+git clone --depth=1 -b dev/shizuka https://github.com/Nicklas373/kernel_xiaomi_lavender .
 
-chmod +x .circle-clang-mido.sh
-bash ./.circle-clang-mido.sh
+wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
+
+chmod +x .circle-clang-lavender.sh
+bash ./.circle-clang-lavender.sh
