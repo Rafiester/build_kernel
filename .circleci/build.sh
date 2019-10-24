2fade85551055c0539973d29473952e9846070ea
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-24 21:17:51 +0700

    circle-ci: test lavender
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,9 +4,9 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# git clone --depth=1 -b test/q-eas https://github.com/Nicklas373/kernel_xiaomi_lavender .
+git clone --depth=1 -b dev/yukina-q https://github.com/Nicklas373/kernel_xiaomi_lavender .
 
-wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
+wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
 
-chmod +x .circle-clang-mido.sh
-bash ./.circle-clang-mido.sh
+chmod +x .circle-clang-lavender.sh
+bash ./.circle-clang-lavender.sh
