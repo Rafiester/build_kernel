db766218d073abc05fe192020d56b49f79554838
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-26 22:42:21 +0700

    test
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-git clone --depth=1 -b pie https://github.com/Nicklas373/kernel_xiaomi_msm8953-3.18-2 .
+git clone --depth=1 -b dev/shizuka https://github.com/Nicklas373/kernel_xiaomi_msm8953-3.18-2 .
 
 wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
 
