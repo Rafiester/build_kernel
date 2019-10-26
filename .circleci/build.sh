f210eb3e0a7ef4b165a20c3224eebdb2717fb7cd
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-26 10:58:45 +0700

    circle-ci: mido: test new clang 10 #2
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-git clone --depth=1 -b 10 https://github.com/Nicklas373/kernel_caf_msm8953-2 .
+git clone --depth=1 -b 10 https://github.com/Nicklas373/kernel_xiaomi_msm8953-2 .
 
 wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
 
