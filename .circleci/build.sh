3858e3e294c0c28536994b4c46598a0ebb2243c6
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-27 09:27:21 +0700

    circle-ci: clone from sh
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,8 +4,6 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-git clone --depth=1 -b new https://github.com/Nicklas373/kernel_xiaomi_msm8953-3.18-2 .
-
 wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
 
 chmod +x .circle-clang-mido.sh
