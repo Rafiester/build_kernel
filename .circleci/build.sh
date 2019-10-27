c7955d6caa87d1cc13677226c7aa0902f11b1053
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-27 11:09:10 +0700

    REVERT: circle-ci: lavender: declare device id's
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,22 +4,16 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Declare Device ID's
-#
-# Device Name
-# 1 = Lavender || 0 = Mido
-#
-DEVICE_NAME="1"
+# Only use this command to compile mido
+# wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
 
-if [ "$DEVICE_NAME" == "0" ];
-	then
-		wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
-		chmod +x .circle-clang-mido.sh
-		bash ./.circle-clang-mido.sh
-elif [ "$DEVICE_NAME" == "1" ];
-	then
-		git clone --depth=1 -b yukina  https://github.com/Nicklas373/kernel_xiaomi_lavender .
-		wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
-		chmod +x .circle-clang-lavender.sh
-		bash ./.circle-clang-lavender.sh
-fi
+# chmod +x .circle-clang-mido.sh
+# bash ./.circle-clang-mido.sh
+
+# Only use this command to compile lavender
+git clone --depth=1 -b yukina  https://github.com/Nicklas373/kernel_xiaomi_lavender .
+
+wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
+
+chmod +x .circle-clang-lavender.sh
+bash ./.circle-clang-lavender.sh
