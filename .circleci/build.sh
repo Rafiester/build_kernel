592372e32c05fffde80012484ba0a9ca93e5e8c8
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-11-12 21:20:26 +0700

    circle-ci: unified script
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,16 +4,20 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Only use this command to compile mido
-# wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
+# Unified aliases
+#
+# 0 = Mido || 1 = Lavender
+#
+CODENAME="0";
 
-# chmod +x .circle-clang-mido.sh
-# bash ./.circle-clang-mido.sh
+if [ "$CODENAME" == "1" ];
+	then
+		# Only clone kernel source earlier for lavender
+		git clone --depth=1 -b toyama  https://github.com/Nicklas373/kernel_xiaomi_lavender .
+fi
 
-# Only use this command to compile lavender
-git clone --depth=1 -b toyama  https://github.com/Nicklas373/kernel_xiaomi_lavender .
+# Clone compiler script & execute it
+wget --output-document=.circle-clang.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang.sh
+chmod +x .circle-clang.sh
+bash ./.circle-clang.sh
 
-wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
-
-chmod +x .circle-clang-lavender.sh
-bash ./.circle-clang-lavender.sh
