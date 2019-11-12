70cfcede9390a74f6c90cae02bc80c492cdd3727
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-11-12 20:03:26 +0700

    circle-ci: lavender: R10 BETA
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,15 +5,15 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Only use this command to compile mido
- wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
+# wget --output-document=.circle-clang-mido.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-mido.sh
 
- chmod +x .circle-clang-mido.sh
- bash ./.circle-clang-mido.sh
+# chmod +x .circle-clang-mido.sh
+# bash ./.circle-clang-mido.sh
 
 # Only use this command to compile lavender
-#git clone --depth=1 -b yukina  https://github.com/Nicklas373/kernel_xiaomi_lavender .
+git clone --depth=1 -b toyama  https://github.com/Nicklas373/kernel_xiaomi_lavender .
 
-#wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
+wget --output-document=.circle-clang-lavender.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang-lavender.sh
 
-#chmod +x .circle-clang-lavender.sh
-#bash ./.circle-clang-lavender.sh
+chmod +x .circle-clang-lavender.sh
+bash ./.circle-clang-lavender.sh
