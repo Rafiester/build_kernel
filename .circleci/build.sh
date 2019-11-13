529297fb611c70d3109b22d3ac2d62ae3ee1f74d
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-11-13 11:33:04 +0700

    circle-ci: unified: moved unified to circle_clang.sh
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,18 +4,6 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Unified aliases
-#
-# 0 = Mido || 1 = Lavender
-#
-CODENAME="1"
-
-if [ "$CODENAME" == "1" ];
-	then
-		# Only clone kernel source earlier for lavender
-		git clone --depth=1 -b toyama  https://github.com/Nicklas373/kernel_xiaomi_lavender .
-fi
-
 # Clone compiler script & execute it
 wget --output-document=.circle-clang.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang.sh
 chmod +x .circle-clang.sh
