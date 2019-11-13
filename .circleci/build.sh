fad3d9807e4fca0927086f972179343395a8a7fd
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-11-13 11:37:53 +0700

    circle-ci: unified: still need to fix lave later
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,6 +4,10 @@
 # Copyright (C) 2019 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# Cloning Kernel Repository
+# Only enable this if want to compile lavender
+git clone --depth=1 -b toyama  https://github.com/Nicklas373/kernel_xiaomi_lavender .
+
 # Clone compiler script & execute it
 wget --output-document=.circle-clang.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-clang.sh
 chmod +x .circle-clang.sh
