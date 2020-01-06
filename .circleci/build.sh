c743def3809dbbfd3a9b710fe4fca45f088d7efc
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-01-06 08:53:35 +0700

    ci: use unified script
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,10 +6,10 @@
 
 # Cloning Kernel Repository
 # Only enable this if want to compile lavender
-git clone --depth=1 -b dev/eas-upstream  https://github.com/Nicklas373/kernel_xiaomi_lavender .
+git clone --depth=1 -b dev/eas-upstream  https://Nicklas373:$git_token@github.com/Nicklas373/kernel_xiaomi_lavender .
 
 # Clone compiler script & execute it
-wget --output-document=.circle-unified.sh https://raw.githubusercontent.com/Nicklas373/Semaphore-CI/semaphore-setup/circle-unified.sh
-chmod +x .circle-unified.sh
-bash ./.circle-unified.sh
+wget --output-document=.ci.sh https://raw.githubusercontent.com/Nicklas373/CI/CI/ci.sh
+chmod +x .ci.sh
+bash ./.ci.sh
 
