155212e2db991ad4224f4a84b45d8a24153dcc71
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-01-07 12:26:53 +0700

    ci: unified: test ion sultan
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,7 +6,7 @@
 
 # Cloning Kernel Repository
 # Only enable this if want to compile lavender
-# git clone --depth=1 -b dev/kasumi  https://github.com/Nicklas373/kernel_xiaomi_lavender .
+git clone --depth=1 -b dev/ion  https://github.com/Nicklas373/kernel_xiaomi_lavender .
 
 # Clone compiler script & execute it
 wget --output-document=.ci.sh https://raw.githubusercontent.com/Nicklas373/CI/CI/ci.sh
