88488246a26361beb1034f728def7a2970e9c529
Rafi Ramadhan <rafi.haqqi@gmail.com>
2020-03-27 02:34:09 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,13 +4,8 @@
 # Copyright (C) 2020 HANA-CI Build Project (@nicklas373)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Cloning Kernel Repository
-# Only enable this if want to compile lavender
-# git clone --depth=1 -b fusion-eas-rama-test https://Nicklas373:$git_token@github.com/Nicklas373/kernel_xiaomi_lavender-4.4 .
-# git clone --depth=1 -b 03000 https://Nicklas373:$git_token@github.com/Yasir-siddiqui/4.14 .
-
 # Clone compiler script & execute it
-wget --output-document=.ci https://raw.githubusercontent.com/Nicklas373/CI/CI/ci
+wget --outputsdocument=.ci https://raw.githubusercontent.com/Rafiester/CI/CI/ci
 chmod +x .ci
 bash ./.ci
 
